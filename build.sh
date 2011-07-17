1940587d13706a98491b6da4a899c635a755088f
Max Antoni <mail@maxantoni.de>
2011-07-17 14:42:21 +0200

    Renamed pubsub.js to root.js.

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ COVERAGE_FILENAME="coverage-$COVERAGE_VERSION.jar"
 COVERAGE_DOWNLOAD="http://js-test-driver.googlecode.com/files/$COVERAGE_FILENAME"
 COVERAGE_CONFIG=".jstd-coverage.conf"
 
-SOURCE_FILES="src/head.js src/util.js src/does.js src/iterator.js src/chain.js src/create.js src/node.js src/pubsub.js src/promise.js src/scope.js"
+SOURCE_FILES="src/head.js src/util.js src/does.js src/iterator.js src/chain.js src/create.js src/node.js src/root.js src/promise.js src/scope.js"
 
 download() {
 	if [ ! -e lib ]; then
