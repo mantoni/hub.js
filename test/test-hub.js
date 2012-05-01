29d7fd2029d80481b52f593b93045b309172c3ea
Maximilian Antoni <mail@maxantoni.de>
2012-05-01 20:54:48 +0200

    Exporting strategy.CONCAT in hub namespace.

--- test/test-hub.js
+++ test/test-hub.js
@@ -1,14 +1,38 @@
 var test      = require('utest');
 var assert    = require('assert');
-var sinon     = require('sinon');
 
 var hub       = require('../lib/hub');
+var strategy  = require('../lib/strategy');
 
 
 test('hub.create', {
 
+
   'should create instance of Hub': function () {
     assert(hub.create() instanceof hub.Hub);
   }
+
+
+});
+
+
+test('hub.LAST', {
+
+
+  'should be same as strategy.LAST': function () {
+    assert.strictEqual(hub.LAST, strategy.LAST);
+  }
+
+
+});
+
+
+test('hub.CONCAT', {
+
+
+  'should be same as strategy.CONCAT': function () {
+    assert.strictEqual(hub.CONCAT, strategy.CONCAT);
+  }
+
 
 });
