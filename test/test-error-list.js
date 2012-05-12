3e3421e4d7b18d2ca273c5f146886e9fe7df2291
Maximilian Antoni <mail@maxantoni.de>
2012-05-12 17:22:17 +0200

    Test cases for error-list. ErrorList is now exposing original errors.

---
+++ test/test-error-list.js
@@ -0,0 +1,33 @@
+var test      = require('utest');
+var assert    = require('assert');
+
+var ErrorList = require('../lib/error-list');
+
+
+test('error-list', {
+
+
+  'should be error': function () {
+    var error = new ErrorList([]);
+
+    assert(error instanceof Error);
+  },
+
+
+  'should be named ErrorList': function () {
+    var error = new ErrorList([]);
+
+    assert.equal(error.name, 'ErrorList');
+  },
+
+
+  'should expose given errors': function () {
+    var errors  = [new TypeError(), new RangeError()];
+
+    var error   = new ErrorList(errors);
+
+    assert.strictEqual(error.errors, errors);
+  }
+
+
+});
