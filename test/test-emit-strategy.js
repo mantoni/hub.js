514d9fd5089c4747baf5fa10fb86016a2558c428
Maximilian Antoni <mail@maxantoni.de>
2012-05-13 20:40:54 +0200

    Added copyright and license header. Added 'use strict';

--- test/test-emit-strategy.js
+++ test/test-emit-strategy.js
@@ -1,3 +1,12 @@
+/**
+ * hub.js
+ *
+ * Copyright (c) 2012 Maximilian Antoni <mail@maxantoni.de>
+ *
+ * @license MIT
+ */
+'use strict';
+
 var test      = require('utest');
 var assert    = require('assert');
 var sinon     = require('sinon');
