ce0bea262c3f0e1bd11a2c955f62fdf92ae7c08a
Maximilian Antoni <mail@maxantoni.de>
2012-06-02 22:20:54 +0200

    Initialize a hub with listeners.
    The hub factory function now accepts arbitrary objects and will install
    all functions as listeners.

--- test/test-hub.js
+++ test/test-hub.js
@@ -9,6 +9,7 @@
 
 var test      = require('utest');
 var assert    = require('assert');
+var sinon     = require('sinon');
 
 var hub       = require('../lib/hub');
 var strategy  = require('../lib/strategy');
@@ -29,7 +30,51 @@ test('hub', {
 
   'should expose strategy.CONCAT': function () {
     assert.strictEqual(hub.CONCAT, strategy.CONCAT);
-  }
+  },
+
+
+  'should call on for each key-function pair': sinon.test(function () {
+    this.stub(hub.Hub.prototype, "on");
+    var listener1 = function () {};
+    var listener2 = function () {};
+
+    hub({
+      'a' : listener1,
+      'b' : listener2
+    });
+
+    sinon.assert.calledTwice(hub.Hub.prototype.on);
+    sinon.assert.calledWith(hub.Hub.prototype.on, 'a', listener1);
+    sinon.assert.calledWith(hub.Hub.prototype.on, 'b', listener2);
+  }),
+
+
+  'should call on for function on prototype': sinon.test(function () {
+    this.stub(hub.Hub.prototype, "on");
+    function Type() {}
+    Type.prototype.test = function () {};
+    var type = new Type();
+
+    hub(type);
+
+    sinon.assert.calledOnce(hub.Hub.prototype.on);
+    sinon.assert.calledWith(hub.Hub.prototype.on, 'test', type.test);
+  }),
+
+
+  'should not call on with non-function values': sinon.test(function () {
+    this.stub(hub.Hub.prototype, "on");
+
+    hub({
+      'a' : 'x',
+      'b' : 123,
+      'c' : true,
+      'd' : {},
+      'e' : new Date()
+    });
+
+    sinon.assert.notCalled(hub.Hub.prototype.on);
+  })
 
 
 });
