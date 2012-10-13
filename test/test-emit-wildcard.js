f0384fae47384f5bfb16b10d4ebd2761d8a0d5a6
Max Antoni <mail@maxantoni.de>
2012-10-13 17:19:04 +0200

    Broadcasting now works with 'before' and 'after'.

--- test/test-emit-wildcard.js
+++ test/test-emit-wildcard.js
@@ -14,6 +14,55 @@ var sinon   = require('sinon');
 var hub     = require('../lib/hub');
 
 
+function matchingListeners(method) {
+  return function () {
+    var spy1 = sinon.spy();
+    var spy2 = sinon.spy();
+    var spy3 = sinon.spy();
+    this.hub[method]('a', spy1);
+    this.hub[method]('a', spy2);
+    this.hub[method]('b', spy3);
+
+    this.hub.emit('*');
+
+    sinon.assert.calledOnce(spy1);
+    sinon.assert.calledOnce(spy2);
+    sinon.assert.calledOnce(spy3);
+  };
+}
+
+function matchingMatchers(method) {
+  return function () {
+    var spy1 = sinon.spy();
+    var spy2 = sinon.spy();
+    var spy3 = sinon.spy();
+    this.hub[method]('**',         spy1);
+    this.hub[method]('foo.*',      spy2);
+    this.hub[method]('foo.*.test', spy3);
+
+    this.hub.emit('foo.**');
+
+    sinon.assert.calledOnce(spy1);
+    sinon.assert.calledOnce(spy2);
+    sinon.assert.calledOnce(spy3);
+  };
+}
+
+function notMatchingMatchers(method) {
+  return function () {
+    var spy = sinon.spy();
+    this.hub[method]('*.bar',    spy);
+    this.hub[method]('**.bar',   spy);
+    this.hub[method]('foo',      spy);
+    this.hub[method]('bar.foo',  spy);
+
+    this.hub.emit('foo.**');
+
+    sinon.assert.notCalled(spy);
+  };
+}
+
+
 test('emit-wildcard', {
 
   before: function () {
@@ -21,48 +70,67 @@ test('emit-wildcard', {
   },
 
 
-  'should invoke matching listeners': function () {
+  'should invoke matching "on" listeners': matchingListeners("on"),
+
+
+  'should invoke matching "on" matchers': matchingMatchers("on"),
+
+
+  'should not invoke not matching "on" matchers': notMatchingMatchers("on"),
+
+
+  'should invoke matching "before" listeners': matchingListeners("before"),
+
+
+  'should invoke matching "before" matchers': matchingMatchers("before"),
+
+
+  'should not invoke not matching "before" matchers':
+    notMatchingMatchers("before"),
+
+
+  'should invoke matching "after" listeners': matchingListeners("after"),
+
+
+  'should invoke matching "after" matchers': matchingMatchers("after"),
+
+
+  'should not invoke not matching "after" matchers':
+    notMatchingMatchers("after"),
+
+
+  'should invoke "before" listeners before "on" listeners': function () {
     var spy1 = sinon.spy();
     var spy2 = sinon.spy();
-    var spy3 = sinon.spy();
-    this.hub.on('a', spy1);
-    this.hub.on('a', spy2);
-    this.hub.on('b', spy3);
+    this.hub.before('test', spy1);
+    this.hub.on('test', spy2);
 
     this.hub.emit('*');
 
-    sinon.assert.calledOnce(spy1);
-    sinon.assert.calledOnce(spy2);
-    sinon.assert.calledOnce(spy3);
+    sinon.assert.callOrder(spy1, spy2);
   },
 
 
-  'should invoke matching matchers': function () {
+  'should invoke "after" listeners after "on" listeners': function () {
     var spy1 = sinon.spy();
     var spy2 = sinon.spy();
-    var spy3 = sinon.spy();
-    this.hub.on('**',         spy1);
-    this.hub.on('foo.*',      spy2);
-    this.hub.on('foo.*.test', spy3);
+    this.hub.on('test', spy1);
+    this.hub.after('test', spy2);
 
-    this.hub.emit('foo.**');
+    this.hub.emit('*');
 
-    sinon.assert.calledOnce(spy1);
-    sinon.assert.calledOnce(spy2);
-    sinon.assert.calledOnce(spy3);
+    sinon.assert.callOrder(spy1, spy2);
   },
 
 
-  'should not invoke not matching matchers': function () {
+  'should invoke "after" listener with result of "on" listener': function () {
     var spy = sinon.spy();
-    this.hub.on('*.bar',    spy);
-    this.hub.on('**.bar',   spy);
-    this.hub.on('foo',      spy);
-    this.hub.on('bar.foo',  spy);
+    this.hub.on('test', function () { return 'Oh, hi!'; });
+    this.hub.after('test', spy);
 
-    this.hub.emit('foo.**');
+    this.hub.emit('*');
 
-    sinon.assert.notCalled(spy);
+    sinon.assert.calledWith(spy, null, "Oh, hi!");
   },
 
 
