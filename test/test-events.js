54a8101fe85cabdb277e54c3e235a5d6f5e19d11
Maximilian Antoni <mail@maxantoni.de>
2012-12-30 17:50:45 +0100

    Renamed emitsEvent to emitsNewListener

--- test/test-events.js
+++ test/test-events.js
@@ -14,7 +14,7 @@ var sinon   = require('sinon');
 var hub     = require('../lib/hub');
 
 
-function emitsEvent(method, event) {
+function emitsNewListener(method, event) {
   return function () {
     var spy       = sinon.spy();
     var listener  = function () {};
@@ -35,21 +35,21 @@ test('events', {
   },
 
 
-  'should emit newListener for on(test)': emitsEvent('on', 'test'),
+  'should emit newListener for on(test)': emitsNewListener('on', 'test'),
 
-  'should emit newListener for on(**)': emitsEvent('on', '**'),
+  'should emit newListener for on(**)': emitsNewListener('on', '**'),
 
-  'should emit newListener for before(test)': emitsEvent('before', 'test'),
+  'should emit newListener for before(test)': emitsNewListener('before', 'test'),
 
-  'should emit newListener for before(**)': emitsEvent('before', '**'),
+  'should emit newListener for before(**)': emitsNewListener('before', '**'),
 
-  'should emit newListener for after(test)': emitsEvent('after', 'test'),
+  'should emit newListener for after(test)': emitsNewListener('after', 'test'),
 
-  'should emit newListener for after(**)': emitsEvent('after', '**'),
+  'should emit newListener for after(**)': emitsNewListener('after', '**'),
 
-  'should emit newListener for once(test)': emitsEvent('once', 'test'),
+  'should emit newListener for once(test)': emitsNewListener('once', 'test'),
 
-  'should emit newListener for once(**)': emitsEvent('once', '**'),
+  'should emit newListener for once(**)': emitsNewListener('once', '**'),
 
 
   'should emit newListener to matchers': function () {
